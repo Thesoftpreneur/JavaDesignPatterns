f95e5eb8fe8ec86ff0e7daf2d250618b99fa3bf3
Maestro3733 <extremewinds@gmail.com>
2020-08-30 01:32:21 +0200

    add deque double queue

---
+++ src/main/java/com/amberlion/otherStuff/Deque/DequeExampleBasics.java
@@ -0,0 +1,25 @@
+package com.amberlion.otherStuff.Deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeExampleBasics {
+    public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+
+        //queue mode
+        deque.offerLast("Element 1");
+        deque.offerLast("Element 2");
+
+        String element1 = deque.pollFirst();
+        String element2 = deque.pollFirst();
+
+        //Stack mode
+        deque.offerFirst("Element 1");
+        deque.offerFirst("Element 2");
+
+        element2 = deque.pollFirst();
+        element1 = deque.pollFirst();
+
+    }
+}
---
+++ src/main/java/com/amberlion/otherStuff/Deque/GenericDeque.java
@@ -0,0 +1,19 @@
+package com.amberlion.otherStuff.Deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class GenericDeque {
+    public static void main(String[] args) {
+        Deque deque = new ArrayDeque();
+
+        deque.offer(Integer.valueOf(123));
+        deque.offer("Hello, Deque");
+
+        Integer element1 = (Integer) deque.poll();
+        String element2 = (String) deque.poll();
+
+        Deque<String> deque2 = new ArrayDeque<>();
+        deque2.offer("Hello, Deque");
+    }
+}
