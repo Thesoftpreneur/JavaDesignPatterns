c3e83d7cb66eb9b6f822dfaf66be328f3d805bee
Maestro3733 <extremewinds@gmail.com>
2020-08-30 02:00:30 +0200

    add deque iterator

---
+++ src/main/java/com/amberlion/otherStuff/Deque/DequeueIteration.java
@@ -0,0 +1,28 @@
+package com.amberlion.otherStuff.Deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class DequeueIteration {
+    public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+
+        deque.offer("element 1");
+        deque.offer("element 2");
+        deque.offer("element 3");
+
+        Iterator<String> iterator = deque.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        for(String element : deque) {
+            System.out.println(element);
+        }
+
+        deque.stream().forEach((element)-> {
+            System.out.println(element);
+        });
+    }
+}
