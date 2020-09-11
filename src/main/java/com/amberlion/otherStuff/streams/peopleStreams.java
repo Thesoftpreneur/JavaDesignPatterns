159ee489bd94e06ca32395658476a30b7af205ee
Maestro3733 <extremewinds@gmail.com>
2020-09-11 23:05:49 +0200

    add people streams

---
+++ src/main/java/com/amberlion/otherStuff/streams/peopleStreams.java
@@ -0,0 +1,11 @@
+package com.amberlion.otherStuff.streams;
+
+import java.util.stream.Stream;
+
+public class peopleStreams {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Hello");
+
+        //Stream<String> stream1
+    }
+}
