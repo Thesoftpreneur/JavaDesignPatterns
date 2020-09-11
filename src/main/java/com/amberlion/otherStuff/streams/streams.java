494ade437d6233a1638367d6b1883c26d047dbfa
Maestro3733 <extremewinds@gmail.com>
2020-09-11 21:59:14 +0200

    simple streams

--- src/main/java/com/amberlion/otherStuff/streams/streams.java
+++ src/main/java/com/amberlion/otherStuff/streams/streams.java
@@ -3,6 +3,7 @@ package com.amberlion.otherStuff.streams;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class streams {
     public static void main(String[] args) {
@@ -12,11 +13,22 @@ public class streams {
                 .map(n -> n * n)
                 .reduce(0, Integer::sum);
 
-        System.out.println("sumOfOddSquares:"+sumOfOddSquares);
+        System.out.println("sumOfOddSquares_:"+sumOfOddSquares);
+
+        int sumOfOddSquares2 = numbers.stream()
+                .filter(n -> n%2 == 1)
+                .map(n -> n * n)
+                .reduce(0, (a, b) -> a+b);
+        System.out.println("sumOfOddSquares2:"+sumOfOddSquares2);
 
         Iterator<Integer> iterator = numbers.stream().iterator();
         while(iterator.hasNext()) {
             System.out.println("Iteration:"+iterator.next());
         }
+
+        Stream<Integer> allNumbers = numbers.stream();
+        System.out.println("Even numbers ->");
+        allNumbers.filter(n-> n%2 == 0).forEach(System.out::print);
+        //System.out.println("Even numbers:" + evenNumbers);
     }
 }
